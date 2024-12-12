cfde6caad379b2cd469f5f82e7cb257623509b5f
Riku Turunen <riku.turunen@student.hamk.fi>
2024-12-12 18:42:36 +0200

    Lisätty Rikun osio
    Riku was here

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Pauliina was here");
+        System.out.println("Riku was here");
     }
 }
